8d6845e35cf89b127f96303581d146b7db38b2b3
CORP\camenjiv <carlo.menjivar@capgemini.com>
2022-11-03 18:44:47 +0100

    Using Lombok

--- src/main/java/com/pluralsight/entity/Ticket.java
+++ src/main/java/com/pluralsight/entity/Ticket.java
@@ -1,13 +1,34 @@
 package com.pluralsight.entity;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String title;
+
     private String description;
 
     @ManyToOne
@@ -15,68 +36,10 @@ public class Ticket {
     private Application application;
 
     @ManyToOne
-    @JoinTable(name ="ticket_release", joinColumns = @JoinColumn(name = "ticket_fk"), inverseJoinColumns = @JoinColumn(name = "release_fk"))
+    @JoinTable(name = "ticket_release", joinColumns = @JoinColumn(name = "ticket_fk"),
+        inverseJoinColumns = @JoinColumn(name = "release_fk"))
     private Release release;
 
     private String status;
 
-    public Ticket() {
-    }
-
-    public Ticket(String title, String description,
-                  Application application, Release release, String status) {
-        this.title = title;
-        this.description = description;
-        this.application = application;
-        this.release = release;
-        this.status = status;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Application getApplication() {
-        return application;
-    }
-
-    public void setApplication(Application application) {
-        this.application = application;
-    }
-
-    public Release getRelease() {
-        return release;
-    }
-
-    public void setRelease(Release release) {
-        this.release = release;
-    }
 }
