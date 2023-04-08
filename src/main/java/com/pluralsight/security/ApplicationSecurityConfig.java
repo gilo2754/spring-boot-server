e9bb27f5a16b3b0dba10b2bae1667f6d13d4036f
Gilo_Menji <gilo.dev@gmail.com>
2023-04-08 19:09:06 +0200

    Auth EndPoint and Cors

--- src/main/java/com/pluralsight/security/ApplicationSecurityConfig.java
+++ src/main/java/com/pluralsight/security/ApplicationSecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -45,11 +50,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http //
+      /*  http //
             .csrf().disable()//
             .authorizeRequests()//
             .antMatchers("/", "index", "/css/*", "/js/*").permitAll()//
-            // The order of the Matchers matters
+      */
+        http.csrf().disable()
+                .cors() // Habilitar CORS
+                .and()
+                .authorizeRequests()
+                .antMatchers("/basicauth/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic();
+
+        // The order of the Matchers matters
             // Role-base
             // .antMatchers("/api/v1/**").hasRole(ApplicationUserRole.STUDENT.name())//
             // .antMatchers("admin/api/v1/**").hasAnyRole(ApplicationUserRole.ADMIN.name())//
@@ -58,12 +73,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             // .hasAuthority(ApplicationUserPermision.USER_DELETE.getPermission())//
             // .antMatchers(HttpMethod.POST, "admin/api/v1/**")
             // .hasAuthority(ApplicationUserPermision.USER_CREATE.getPermission())//
-            .anyRequest().authenticated().and().httpBasic();
 
-        http.csrf().disable();
+       // http.csrf().disable();
         http.headers().frameOptions().disable();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Origen permitido
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
+        configuration.setExposedHeaders(Arrays.asList("authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
