f130b0dd5c822f9e8f90a805b50108ef9bfb9a34
gilo2754@gmail.com <gilo2754@gmail.com>
2023-09-22 17:57:11 +0200

    Appointment mini refactoring

--- src/main/java/com/pluralsight/controller/AppointmentController.java
+++ src/main/java/com/pluralsight/controller/AppointmentController.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/appointment")
 @AllArgsConstructor
 public class AppointmentController {
     private AppointmentService appointmentService;
@@ -30,7 +30,7 @@ public class AppointmentController {
     private ClinicService clinicService;
 
     //TODO: show just the appointments from the actual user
-    @GetMapping("/appointment")
+    @GetMapping
     public ResponseEntity<List<Appointment>> getMyAppointments(){//Authentication authentication) {
         //Long personId = extractPersonIdFromAuthentication(authentication);
         //List<Appointment> userAppointments = appointmentService.listAppointmentsByPersonId(personId);
@@ -61,7 +61,7 @@ public class AppointmentController {
      * @param updatedAppointment
      * @return
      */
-    @PutMapping("update")
+    @PutMapping("/update")
     public ResponseEntity<?> updateAppointment(@RequestBody Appointment updatedAppointment) {
         // Check if the appointmentId is present in the updatedAppointment
         Long appointmentId = updatedAppointment.getAppointment_id();
@@ -99,18 +99,53 @@ public class AppointmentController {
         return ResponseEntity.ok(updated);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Appointment> getAppoitment(@PathVariable("id") long id) {
+        try {
+            return new ResponseEntity<Appointment>(this.appointmentService.getAppointmentById(id),
+                HttpStatus.OK);
+        } catch (AppointmentNotFoundException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Appointment Not Found");
+        }
+    }
 
+    @PostMapping("/create")
+    public ResponseEntity<String> createAppointment(@RequestBody Appointment appointment) {
+        try {
+            appointmentService.createAppointment(appointment);
+            return ResponseEntity.ok("Cita médica creada exitosamente.");
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (ClinicNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Clinic> deteleAppointment(@PathVariable("id") long id) {
+        this.appointmentService.deleteAppointment(id);
+        return ResponseEntity.noContent().build();
+    }
 
+    @GetMapping("/clinic/{clinicId}")
+    public ResponseEntity<?> getAppointmentsByClinic(@PathVariable Long clinicId) {
+        List<Appointment> appointments = appointmentService.findByClinic(clinicId);
 
+        if (appointments.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No appointments found for clinic with ID: " + clinicId);
+        } else {
+            return ResponseEntity.ok(appointments);
+        }
+    }
 
-/**
- * Extracts the personId from the authentication.
- * Finds the personId by username and returns the appointments from this user.
- *
- * @param authentication the authentication object representing the currently authenticated user
- * @return the list of appointments for the authenticated user
- */
+    /**
+     * Extracts the personId from the authentication.
+     * Finds the personId by username and returns the appointments from this user.
+     *
+     * @param authentication the authentication object representing the currently authenticated user
+     * @return the list of appointments for the authenticated user
+     */
     private Long extractPersonIdFromAuthentication(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String username = userDetails.getUsername();
@@ -123,47 +158,4 @@ public class AppointmentController {
 
         return personIdLoggedPerson;
     }
-
-    @GetMapping("/appointment/{id}")
-    public ResponseEntity<Appointment> getAppoitment(@PathVariable("id") long id) {
-        try {
-            return new ResponseEntity<Appointment>(this.appointmentService.getAppointmentById(id),
-                HttpStatus.OK);
-        } catch (AppointmentNotFoundException exception) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Appointment Not Found");
-        }
-    }
-
-    @PostMapping("/appointment/create")
-    public ResponseEntity<String> createAppointment(@RequestBody Appointment appointment) {
-        try {
-            appointmentService.createAppointment(appointment);
-            return ResponseEntity.ok("Cita médica creada exitosamente.");
-        } catch (UserNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        } catch (ClinicNotFoundException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-
-
-
-    @DeleteMapping("/appointment/{id}")
-    public ResponseEntity<Clinic> deteleAppointment(@PathVariable("id") long id) {
-        this.appointmentService.deleteAppointment(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/appointment/clinic/{clinicId}")
-    public ResponseEntity<?> getAppointmentsByClinic(@PathVariable Long clinicId) {
-        List<Appointment> appointments = appointmentService.findByClinic(clinicId);
-
-        if (appointments.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("No appointments found for clinic with ID: " + clinicId);
-        } else {
-            return ResponseEntity.ok(appointments);
-        }
-    }
-
 }
