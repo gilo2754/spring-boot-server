177fbc6f6af10c599a5cfedb7cb044cdb5bd6b4e
gilo2754@gmail.com <gilo2754@gmail.com>
2023-10-05 19:21:41 +0200

    https://youtu.be/avYiyEJ6UyE?si=AXq7ld-u-I-2ySLI&t=458

--- src/main/java/com/pluralsight/FundamentalsApplication.java
+++ src/main/java/com/pluralsight/FundamentalsApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(ConfigProperties.class)
 public class FundamentalsApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentalsApplication.class, args);
+		//FundamentalsApplication application = new FundamentalsApplication();
+
 	}
+
+
 }
